0cd48a01d23c4cd3d79987d65803bd4a73568640
Langning Chen <chenlangning2009@outlook.com>
2025-06-21 17:27:50 +0800

    misc: Update copyright information

--- src/base-page.js
+++ src/base-page.js
@@ -1,19 +1,19 @@
 // Copyright (C) 2025 Langning Chen
 // 
-// This file is part of test_example.
+// This file is part of miniapp.
 // 
-// test_example is free software: you can redistribute it and/or modify
+// miniapp is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// test_example is distributed in the hope that it will be useful,
+// miniapp is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
-// along with test_example.  If not, see <https://www.gnu.org/licenses/>.
+// along with miniapp.  If not, see <https://www.gnu.org/licenses/>.
 
 const DEBUG = false
 function _collectFalconEventIds(name, callback)
