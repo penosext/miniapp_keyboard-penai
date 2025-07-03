9f1cc4a7e3e2201538666b24f158c79318b76b90
Langning Chen <chenlangning2009@outlook.com>
2025-07-03 22:14:34 +0800

    fix: Remove useless toolchain file

--- jsapi/toolchain/arm-buildroot-linux-gnueabihf/lib/gcc/arm-buildroot-linux-gnueabihf/7.3.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "./configure --prefix=/opt/armv7-eabihf--glibc--stable-2018.11-1 --sysconfdir=/opt/armv7-eabihf--glibc--stable-2018.11-1/etc --enable-static --target=arm-buildroot-linux-gnueabihf --with-sysroot=/opt/armv7-eabihf--glibc--stable-2018.11-1/arm-buildroot-linux-gnueabihf/sysroot --disable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --with-gmp=/opt/armv7-eabihf--glibc--stable-2018.11-1 --with-mpc=/opt/armv7-eabihf--glibc--stable-2018.11-1 --with-mpfr=/opt/armv7-eabihf--glibc--stable-2018.11-1 --with-pkgversion='Buildroot 2018.08.1-00003-g576b333' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --enable-tls --disable-libmudflap --enable-threads --without-isl --without-cloog --disable-decimal-float --with-abi=aapcs-linux --with-cpu=cortex-a9 --with-fpu=vfpv3-d16 --with-float=hard --with-mode=arm --enable-languages=c,c++ --with-build-time-tools=/opt/armv7-eabihf--glibc--stable-2018.11-1/arm-buildroot-linux-gnueabihf/bin --enable-shared --disable-libgomp";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "cortex-a9" }, { "float", "hard" }, { "mode", "arm" }, { "fpu", "vfpv3-d16" }, { "tls", "gnu" } };
